09bd2226e1e0b0eccd208bfa0dbbcceaa0fd58b9
trevormangahas <trevormangahas@gmail.com>
2022-02-23 13:38:11 -0500

    added console.logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert!, Javascirpt is running!");
+// this creats a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// This will do math and log 20
+console.log(10+10);
+
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+
+
+
+
+// fight();
